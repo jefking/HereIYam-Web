d7cffcef8ac2b003fc74603ce784ffb6ad0af1b6
Jef King <jefkin@microsoft.com>
2015-10-23 11:20:37 -0600

    site

---
+++ app/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(app.Startup))]
+namespace app
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
