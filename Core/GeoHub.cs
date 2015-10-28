2f05b08bf324b90864720027dc8783170efbd9a0
Jef King <jef@agilebusinesscloud.com>
2015-10-27 20:38:26 -0700

    more better

--- Core/GeoHub.cs
+++ Core/GeoHub.cs
@@ -6,9 +6,9 @@
     [HubName("geo")]
     public class GeoHub : Hub
     {
-        public void MyLocation(double lat, double lng)
+        public void MyLocation(double lat, double lng, string notification = null)
         {
-            Clients.All.multicast(lat, lng);
+            Clients.All.multicast(lat, lng, notification);
         }
     }
 }
